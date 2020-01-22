7aab7d84843a0444b5b42057758e5db87735003e
Alessio Libardi <alessio-libardi@gmail.com>
2020-01-22 20:40:49 +0100

    Added admin application

--- apps/ui/admin/src/app/app.component.ts
+++ apps/ui/admin/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ui-portfolio-root',
+  selector: 'ui-admin-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ui-portfolio';
+  title = 'ui-admin';
 }
